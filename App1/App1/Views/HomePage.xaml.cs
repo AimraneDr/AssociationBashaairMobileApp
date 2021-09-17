c2818a25641d98ecc649a0dac23847eaf4fd634f
Aimrane Draou <aimranedr@gmail.com>
2021-09-17 22:59:36 +0100

    reCreate the project

---
+++ App1/App1/Views/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        int i = 0;
+        public HomePage()
+        {
+            InitializeComponent();
+            main_label.Text = $"integer : {i}";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            i++;
+            main_label.Text = $"integer : {i}";
+        }
+    }
+}
